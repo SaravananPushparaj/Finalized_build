189d2907d1d3993beb314ee50f1c67e4704c96ec
unknown <saravananpushparaj@gmail.com>
2015-11-06 13:15:14 +0530

    v56

--- src/test/java/Framework/DataProvider_EMI.java
+++ src/test/java/Framework/DataProvider_EMI.java
@@ -11,7 +11,7 @@ public class DataProvider_EMI {
 	{
 		Object[][] Obj= new Object[2][4];
 		
-		Obj[0][0]="20000";
+		Obj[0][0]="16000";
 		Obj[0][1]="12";
 		Obj[0][2]="60";
 		Obj[0][3]="232.44";
